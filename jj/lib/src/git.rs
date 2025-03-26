glehmann/jj

git: forward rejection reason on push
We've been finding that a lot of bug reports on `jj git push` come from
sub-standard error reporting on the reasons the failure happens.

It can come from a number of places:
 - hook failure
 - remote branch protection
 - git config

This commit forwards the reason as explained by the ouptut of git push
to help users figure out what is happening.

lib/src/git.rs
@@ -107,10 +107,10 @@ pub enum GitRefKind {
 pub struct GitPushStats {
     /// reference accepted by the remote
     pub pushed: Vec<String>,
-    /// rejected reference, due to lease failure
-    pub rejected: Vec<String>,
-    /// reference rejected by the remote
-    pub remote_rejected: Vec<String>,
+    /// rejected reference, due to lease failure, with an optional reason
+    pub rejected: Vec<(String, Option<String>)>,
+    /// reference rejected by the remote, with an optional reason
+    pub remote_rejected: Vec<(String, Option<String>)>,
 }
 
 impl GitPushStats {
@@ -2494,14 +2494,18 @@ fn git2_push_refs(
     for failed_update in &failed_push_negotiations {
         remaining_remote_refs.remove(failed_update.as_str());
     }
-    let mut remote_rejected: Vec<_> = remaining_remote_refs
+    let rejected: Vec<_> = failed_push_negotiations
         .into_iter()
         .sorted()
-        .map(|x| x.to_string())
+        .map(|name| (name, None))
         .collect();
-    remote_rejected.sort();
+    let remote_rejected: Vec<_> = remaining_remote_refs
+        .into_iter()
+        .sorted()
+        .map(|name| (name.to_string(), None))
+        .collect();
 
-    let push_stats = if !failed_push_negotiations.is_empty() {
+    let push_stats = if !rejected.is_empty() {
         // If the push negotiation returned an error, `remote.push` would not
         // have pushed anything and would have returned an error, as expected.
         // However, the error it returns is not necessarily the error we'd
@@ -2510,9 +2514,8 @@ fn git2_push_refs(
         // information. See https://github.com/rust-lang/git2-rs/issues/1042.
 
         assert!(push_result.is_err());
-        failed_push_negotiations.sort();
         GitPushStats {
-            rejected: failed_push_negotiations,
+            rejected,
             remote_rejected,
             ..Default::default()
         }

lib/src/git_subprocess.rs
@@ -439,7 +439,7 @@ fn parse_ref_pushes(stdout: &[u8]) -> Result<GitPushStats, GitSubprocessError> {
         )));
     }
 
-    let mut push_result = GitPushStats::default();
+    let mut push_stats = GitPushStats::default();
     for (idx, line) in stdout
         .lines()
         .skip(1)
@@ -481,14 +481,23 @@ fn parse_ref_pushes(stdout: &[u8]) -> Result<GitPushStats, GitSubprocessError> {
             //  * for a successfully pushed new ref
             //  =  for a ref that was up to date and did not need pushing.
             b"+" | b"-" | b"*" | b"=" | b" " => {
-                push_result.pushed.push(reference);
+                push_stats.pushed.push(reference);
             }
             // ! for a ref that was rejected or failed to push; and
             b"!" => {
-                if summary.starts_with_str("[remote rejected]") {
-                    push_result.remote_rejected.push(reference);
+                if let Some(reason) = summary.strip_prefix(b"[remote rejected]") {
+                    let reason = reason
+                        .strip_prefix(b" (")
+                        .and_then(|r| r.strip_suffix(b")"))
+                        .map(|x| x.to_str_lossy().to_string());
+                    push_stats.remote_rejected.push((reference, reason));
                 } else {
-                    push_result.rejected.push(reference);
+                    let reason = summary
+                        .split_once_str("]")
+                        .and_then(|(_, reason)| reason.strip_prefix(b" ("))
+                        .and_then(|r| r.strip_suffix(b")"))
+                        .map(|x| x.to_str_lossy().to_string());
+                    push_stats.rejected.push((reference, reason));
                 }
             }
             unknown => {
@@ -502,7 +511,7 @@ fn parse_ref_pushes(stdout: &[u8]) -> Result<GitPushStats, GitSubprocessError> {
         }
     }
 
-    Ok(push_result)
+    Ok(push_stats)
 }
 
 // on Ok, return a tuple with
@@ -745,7 +754,9 @@ and the repository exists. "###;
  \tdeadbeef:refs/heads/bookmark4\tabcd..dead
 =\tdeadbeef:refs/heads/bookmark5\tabcd..abcd
 !\tdeadbeef:refs/heads/bookmark6\t[rejected] (failure lease)
-!\tdeadbeef:refs/heads/bookmark7\t[remote rejected] (hook failure)
+!\tdeadbeef:refs/heads/bookmark7\t[rejected]
+!\tdeadbeef:refs/heads/bookmark8\t[remote rejected] (hook failure)
+!\tdeadbeef:refs/heads/bookmark9\t[remote rejected]
 Done";
     const SAMPLE_OK_STDERR: &[u8] = b"";
 
@@ -833,8 +844,26 @@ Done";
                 "refs/heads/bookmark5".to_string(),
             ]
         );
-        assert_eq!(rejected, vec!["refs/heads/bookmark6".to_string()]);
-        assert_eq!(remote_rejected, vec!["refs/heads/bookmark7".to_string()]);
+        assert_eq!(
+            rejected,
+            vec![
+                (
+                    "refs/heads/bookmark6".to_string(),
+                    Some("failure lease".to_string())
+                ),
+                ("refs/heads/bookmark7".to_string(), None),
+            ]
+        );
+        assert_eq!(
+            remote_rejected,
+            vec![
+                (
+                    "refs/heads/bookmark8".to_string(),
+                    Some("hook failure".to_string())
+                ),
+                ("refs/heads/bookmark9".to_string(), None)
+            ]
+        );
         assert!(parse_ref_pushes(SAMPLE_OK_STDERR).is_err());
     }
 

0WD0/jjosh

jj/lib/src/git.rs
@@ -110,9 +110,9 @@ pub struct GitPushStats {
     /// reference accepted by the remote
     pub pushed: Vec<String>,
     /// rejected reference, due to lease failure, with an optional reason
-    pub rejected: Vec<String>,
+    pub rejected: Vec<(String, Option<String>)>,
     /// reference rejected by the remote, with an optional reason
-    pub remote_rejected: Vec<String>,
+    pub remote_rejected: Vec<(String, Option<String>)>,
 }
 
 impl GitPushStats {
@@ -2629,11 +2629,15 @@ fn git2_push_refs(
     for failed_update in &failed_push_negotiations {
         remaining_remote_refs.remove(failed_update.as_str());
     }
-    let rejected: Vec<_> = failed_push_negotiations.into_iter().sorted().collect();
+    let rejected: Vec<_> = failed_push_negotiations
+        .into_iter()
+        .sorted()
+        .map(|name| (name, None))
+        .collect();
     let remote_rejected: Vec<_> = remaining_remote_refs
         .into_iter()
         .sorted()
-        .map(str::to_owned)
+        .map(|name| (name.to_owned(), None))
         .collect();
     pushed_refs.sort();
 

jj/lib/src/git_subprocess.rs
@@ -493,10 +493,19 @@ fn parse_ref_pushes(stdout: &[u8]) -> Result<GitPushStats, GitSubprocessError> {
             }
             // ! for a ref that was rejected or failed to push; and
             b"!" => {
-                if summary.starts_with_str("[remote rejected]") {
-                    push_stats.remote_rejected.push(reference);
+                if let Some(reason) = summary.strip_prefix(b"[remote rejected]") {
+                    let reason = reason
+                        .strip_prefix(b" (")
+                        .and_then(|r| r.strip_suffix(b")"))
+                        .map(|x| x.to_str_lossy().into_owned());
+                    push_stats.remote_rejected.push((reference, reason));
                 } else {
-                    push_stats.rejected.push(reference);
+                    let reason = summary
+                        .split_once_str("]")
+                        .and_then(|(_, reason)| reason.strip_prefix(b" ("))
+                        .and_then(|r| r.strip_suffix(b")"))
+                        .map(|x| x.to_str_lossy().into_owned());
+                    push_stats.rejected.push((reference, reason));
                 }
             }
             unknown => {
@@ -753,7 +762,9 @@ and the repository exists. "###;
  \tdeadbeef:refs/heads/bookmark4\tabcd..dead
 =\tdeadbeef:refs/heads/bookmark5\tabcd..abcd
 !\tdeadbeef:refs/heads/bookmark6\t[rejected] (failure lease)
-!\tdeadbeef:refs/heads/bookmark7\t[remote rejected] (hook failure)
+!\tdeadbeef:refs/heads/bookmark7\t[rejected]
+!\tdeadbeef:refs/heads/bookmark8\t[remote rejected] (hook failure)
+!\tdeadbeef:refs/heads/bookmark9\t[remote rejected]
 Done";
     const SAMPLE_OK_STDERR: &[u8] = b"";
 
@@ -841,8 +852,26 @@ Done";
                 "refs/heads/bookmark5".to_string(),
             ]
         );
-        assert_eq!(rejected, vec!["refs/heads/bookmark6".to_string()]);
-        assert_eq!(remote_rejected, vec!["refs/heads/bookmark7".to_string()]);
+        assert_eq!(
+            rejected,
+            vec![
+                (
+                    "refs/heads/bookmark6".to_string(),
+                    Some("failure lease".to_string())
+                ),
+                ("refs/heads/bookmark7".to_string(), None),
+            ]
+        );
+        assert_eq!(
+            remote_rejected,
+            vec![
+                (
+                    "refs/heads/bookmark8".to_string(),
+                    Some("hook failure".to_string())
+                ),
+                ("refs/heads/bookmark9".to_string(), None)
+            ]
+        );
         assert!(parse_ref_pushes(SAMPLE_OK_STDERR).is_err());
     }
 