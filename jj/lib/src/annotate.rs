mernen/jj

annotate: point to commit outside of domain when origin cannot be tracked
As a consequence, in case of error, rather than pointing at the last
commit where this line was seen, it points at the first commit where
the annotation process should continue if the domain were expanded.

This is better aligned with what `git blame` returns.

lib/src/annotate.rs
@@ -339,9 +339,9 @@ fn process_commit(
         } else if parent_edge.edge_type == GraphEdgeType::Missing {
             // If an omitted parent had the file, leave these lines unresolved.
             // The origin of the unresolved lines is represented as
-            // Err(root_commit_id).
+            // Err(parent_commit_id).
             for &(_, starting_line_number) in &parent_source.line_map {
-                state.original_line_map[starting_line_number] = Err(current_commit_id.clone());
+                state.original_line_map[starting_line_number] = Err(parent_commit_id.clone());
             }
             state.num_unresolved_roots += 1;
         }

0WD0/jjosh

jj/lib/src/annotate.rs
@@ -64,8 +64,8 @@ impl FileAnnotation {
     ///
     /// For each line, `Ok(commit_id)` points to the originator commit of the
     /// line. If no originator commit was found within the domain,
-    /// `Err(commit_id)` should be set. It points to the root (or boundary)
-    /// commit where the line exists.
+    /// `Err(commit_id)` should be set. It points to the commit outside of the
+    /// domain where the search stopped.
     ///
     /// The `line` includes newline character.
     pub fn lines(&self) -> impl Iterator<Item = (Result<&CommitId, &CommitId>, &BStr)> {
@@ -339,9 +339,9 @@ fn process_commit(
         } else if parent_edge.edge_type == GraphEdgeType::Missing {
             // If an omitted parent had the file, leave these lines unresolved.
             // The origin of the unresolved lines is represented as
-            // Err(root_commit_id).
+            // Err(parent_commit_id).
             for &(_, original_line_number) in &parent_source.line_map {
-                state.original_line_map[original_line_number] = Err(current_commit_id.clone());
+                state.original_line_map[original_line_number] = Err(parent_commit_id.clone());
             }
             state.num_unresolved_roots += 1;
         }