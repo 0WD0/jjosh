MichaelChirico/jj

cli: add redo command
This is the natural counterpart of `jj undo`. Adding `jj redo` closes
the last gap in functionality to the "undo" features of typical GUI
applications. This is much more intuitive to new users and useful than
the previous undo semantics. (Which are preserved as `jj op revert`.)

Related feature request "jj undo ergonomics":
https://github.com/jj-vcs/jj/issues/3700

cli/tests/runner.rs
@@ -58,6 +58,7 @@ mod test_op_revert_command;
 mod test_operations;
 mod test_parallelize_command;
 mod test_rebase_command;
+mod test_redo;
 mod test_repo_change_report;
 mod test_resolve_command;
 mod test_restore_command;

0WD0/jjosh

jj/cli/tests/runner.rs
@@ -74,7 +74,7 @@ mod test_status_command;
 mod test_tag_command;
 mod test_templater;
 mod test_touch_command;
-mod test_undo;
+mod test_undo_redo_commands;
 mod test_util_command;
 mod test_working_copy;
 mod test_workspaces;