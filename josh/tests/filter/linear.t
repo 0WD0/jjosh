josh-project/josh

Implement history linearization as meta option

tests/filter/linear.t
@@ -33,7 +33,11 @@
 
   $ josh-filter -s :linear refs/heads/master --update refs/heads/filtered
   d24e7038b232dc1bd6d803d211e92039229375b4
-  [4] :linear
+  [4] :~(
+      parents="linear"
+  )[
+      :/
+  ]
   [4] sequence_number
 
   $ git log --graph --pretty=%s refs/heads/filtered
@@ -61,7 +65,11 @@
   $ josh-filter -s :linear refs/heads/master --update refs/heads/filtered --reverse
   65fb0dcfe9fd24ab4d7027ff1359bd44847bd21a
   d24e7038b232dc1bd6d803d211e92039229375b4
-  [4] :linear
+  [4] :~(
+      parents="linear"
+  )[
+      :/
+  ]
   [4] sequence_number
 
   $ git log --graph --pretty=%s refs/heads/master

tests/filter/rev.t
@@ -169,12 +169,16 @@
   [2] :rev(0000000000000000000000000000000000000000:prefix=x/y,975d4c4975912729482cc864d321c5196a969271:prefix=x/z)
   [2] :rev(0000000000000000000000000000000000000000:prefix=x/y,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [2] :rev(0000000000000000000000000000000000000000:prefix=x/z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
-  [5] :linear
   [5] :prefix=y
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/y)
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/y,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [5] :rev(e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
+  [5] :~(
+      parents="linear"
+  )[
+      :/
+  ]
   [6] :prefix=x
   [11] sequence_number
   $ git log --graph --decorate --pretty=%H:%T refs/heads/filtered
@@ -212,12 +216,16 @@
   [2] :rev(0000000000000000000000000000000000000000:prefix=x/z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [2] :rev(0000000000000000000000000000000000000000:prefix=y,0b4cf6c9efbbda1eada39fa9c1d21d2525b027bb:prefix=z)
   [3] :rev(0000000000000000000000000000000000000000:prefix=x,0b4cf6c9efbbda1eada39fa9c1d21d2525b027bb:prefix=z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=y)
-  [5] :linear
   [5] :prefix=y
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/y)
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/y,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [5] :rev(e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
+  [5] :~(
+      parents="linear"
+  )[
+      :/
+  ]
   [6] :prefix=x
   [12] sequence_number
 

0WD0/jjosh

josh/tests/filter/linear.t
@@ -33,7 +33,11 @@
 
   $ josh-filter -s :linear refs/heads/master --update refs/heads/filtered
   d24e7038b232dc1bd6d803d211e92039229375b4
-  [4] :linear
+  [4] :~(
+      graph="linear"
+  )[
+      :/
+  ]
   [4] sequence_number
 
   $ git log --graph --pretty=%s refs/heads/filtered
@@ -60,7 +64,11 @@
 
   $ josh-filter -s :linear refs/heads/master --update refs/heads/filtered --reverse
   65fb0dcfe9fd24ab4d7027ff1359bd44847bd21a
-  [4] :linear
+  [4] :~(
+      graph="linear"
+  )[
+      :/
+  ]
   [4] sequence_number
 
   $ git log --graph --pretty=%s refs/heads/master

josh/tests/filter/rev.t
@@ -169,12 +169,16 @@
   [2] :rev(0000000000000000000000000000000000000000:prefix=x/y,975d4c4975912729482cc864d321c5196a969271:prefix=x/z)
   [2] :rev(0000000000000000000000000000000000000000:prefix=x/y,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [2] :rev(0000000000000000000000000000000000000000:prefix=x/z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
-  [5] :linear
   [5] :prefix=y
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/y)
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/y,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [5] :rev(e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
+  [5] :~(
+      graph="linear"
+  )[
+      :/
+  ]
   [6] :prefix=x
   [11] sequence_number
   $ git log --graph --decorate --pretty=%H:%T refs/heads/filtered
@@ -212,12 +216,16 @@
   [2] :rev(0000000000000000000000000000000000000000:prefix=x/z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [2] :rev(0000000000000000000000000000000000000000:prefix=y,0b4cf6c9efbbda1eada39fa9c1d21d2525b027bb:prefix=z)
   [3] :rev(0000000000000000000000000000000000000000:prefix=x,0b4cf6c9efbbda1eada39fa9c1d21d2525b027bb:prefix=z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=y)
-  [5] :linear
   [5] :prefix=y
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/y)
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/y,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [5] :rev(975d4c4975912729482cc864d321c5196a969271:prefix=x/z,e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
   [5] :rev(e707f76bb6a1390f28b2162da5b5eb6933009070:prefix=x/y)
+  [5] :~(
+      graph="linear"
+  )[
+      :/
+  ]
   [6] :prefix=x
   [12] sequence_number
 