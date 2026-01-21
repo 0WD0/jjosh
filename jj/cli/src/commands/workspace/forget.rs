jj-vcs/jj

workspaces: Allow retrieving the path of another workspace
This allows `jj workspace root` command to take an optional workspace
arg to print the root directory of that workspace, while defaulting to
the current workspace.

We allow this by creating a new `workspace_store` which has a separate
entry for each workspace containing its name and root directory. This
store is created during initiation of the repo and is maintained
whenever `add`, `forget` & `rename` workspace subcommands are executed.

CAUTION: Moving a workspace path will not sync the path stored in the
workspace store.

CAUTION: Specifying `jj workspace root` with a workspace created before
this will result in an error.

cli/src/commands/workspace/forget.rs
@@ -15,6 +15,8 @@
 use clap_complete::ArgValueCandidates;
 use itertools::Itertools as _;
 use jj_lib::ref_name::WorkspaceNameBuf;
+use jj_lib::workspace_store::SimpleWorkspaceStore;
+use jj_lib::workspace_store::WorkspaceStore as _;
 use tracing::instrument;
 
 use crate::cli_util::CommandHelper;
@@ -42,6 +44,7 @@ pub fn cmd_workspace_forget(
     args: &WorkspaceForgetArgs,
 ) -> Result<(), CommandError> {
     let mut workspace_command = command.workspace_helper(ui)?;
+    let repo_path = workspace_command.repo_path().to_path_buf();
 
     let wss = if args.workspaces.is_empty() {
         vec![workspace_command.workspace_name().to_owned()]
@@ -63,8 +66,14 @@ pub fn cmd_workspace_forget(
     // bundle every workspace forget into a single transaction, so that e.g.
     // undo correctly restores all of them at once.
     let mut tx = workspace_command.start_transaction();
-    wss.iter()
-        .try_for_each(|ws| tx.repo_mut().remove_wc_commit(ws))?;
+
+    for ws in &wss {
+        tx.repo_mut().remove_wc_commit(ws)?;
+    }
+
+    SimpleWorkspaceStore::load(&repo_path)?
+        .forget(&wss.iter().map(|x| x.as_ref()).collect::<Vec<_>>())?;
+
     let description = if let [ws] = wss.as_slice() {
         format!("forget workspace {}", ws.as_symbol())
     } else {

lib/gen-protos/src/main.rs
@@ -22,6 +22,7 @@ fn main() -> Result<()> {
         "local_working_copy.proto",
         "simple_op_store.proto",
         "simple_store.proto",
+        "workspace_store.proto",
     ];
 
     let root = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap();

lib/src/protos/mod.rs
@@ -14,3 +14,6 @@ pub mod simple_op_store {
 pub mod simple_store {
     include!("simple_store.rs");
 }
+pub mod workspace_store {
+    include!("workspace_store.rs");
+}

0WD0/jjosh

jj/cli/src/commands/workspace/forget.rs
@@ -15,6 +15,8 @@
 use clap_complete::ArgValueCandidates;
 use itertools::Itertools as _;
 use jj_lib::ref_name::WorkspaceNameBuf;
+use jj_lib::workspace_store::SimpleWorkspaceStore;
+use jj_lib::workspace_store::WorkspaceStore as _;
 use tracing::instrument;
 
 use crate::cli_util::CommandHelper;
@@ -70,12 +72,18 @@ pub fn cmd_workspace_forget(
         return Ok(());
     }
 
+    let workspace_store = SimpleWorkspaceStore::load(workspace_command.repo_path())?;
+
     // bundle every workspace forget into a single transaction, so that e.g.
     // undo correctly restores all of them at once.
     let mut tx = workspace_command.start_transaction();
-    forget_ws
-        .iter()
-        .try_for_each(|ws| tx.repo_mut().remove_wc_commit(ws))?;
+
+    for ws in &forget_ws {
+        tx.repo_mut().remove_wc_commit(ws)?;
+    }
+
+    workspace_store.forget(&forget_ws.iter().map(|x| x.as_ref()).collect::<Vec<_>>())?;
+
     let description = if let [ws] = forget_ws.as_slice() {
         format!("forget workspace {}", ws.as_symbol())
     } else {

jj/lib/gen-protos/src/main.rs
@@ -23,6 +23,7 @@ fn main() -> Result<()> {
         "secure_config.proto",
         "simple_op_store.proto",
         "simple_store.proto",
+        "simple_workspace_store.proto",
     ];
 
     let root = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap();

jj/lib/src/protos/mod.rs
@@ -17,3 +17,6 @@ pub mod simple_op_store {
 pub mod simple_store {
     include!("simple_store.rs");
 }
+pub mod simple_workspace_store {
+    include!("simple_workspace_store.rs");
+}