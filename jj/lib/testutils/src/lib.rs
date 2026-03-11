Vampire/jj

testutils: add helper functions for manipulating copy history

lib/testutils/src/lib.rs
@@ -31,6 +31,7 @@ use jj_lib::backend::Backend;
 use jj_lib::backend::BackendInitError;
 use jj_lib::backend::ChangeId;
 use jj_lib::backend::CommitId;
+use jj_lib::backend::CopyHistory;
 use jj_lib::backend::CopyId;
 use jj_lib::backend::FileId;
 use jj_lib::backend::MergedTreeId;
@@ -423,6 +424,7 @@ impl TestTreeBuilder {
             path: path.to_owned(),
             contents: contents.as_ref().to_vec(),
             executable: false,
+            copy_history: None,
         }
     }
 
@@ -452,11 +454,17 @@ pub struct TestTreeFileEntryBuilder<'a> {
     path: RepoPathBuf,
     contents: Vec<u8>,
     executable: bool,
+    copy_history: Option<CopyHistory>,
 }
 
 impl TestTreeFileEntryBuilder<'_> {
     pub fn executable(mut self, executable: bool) -> Self {
         self.executable = executable;
+        self
+    }
+
+    pub fn copy_history(mut self, copy_history: &CopyHistory) -> Self {
+        self.copy_history = Some(copy_history.clone());
         self
     }
 }
@@ -470,12 +478,22 @@ impl Drop for TestTreeFileEntryBuilder<'_> {
             .block_on()
             .unwrap();
         let path = std::mem::replace(&mut self.path, RepoPathBuf::root());
+        let copy_id = if let Some(copy_history) = self.copy_history.as_ref() {
+            self.tree_builder
+                .store()
+                .backend()
+                .write_copy(copy_history)
+                .block_on()
+                .unwrap()
+        } else {
+            CopyId::placeholder()
+        };
         self.tree_builder.set(
             path,
             TreeValue::File {
                 id,
                 executable: self.executable,
-                copy_id: CopyId::placeholder(),
+                copy_id,
             },
         );
     }
@@ -511,6 +529,17 @@ pub fn create_tree(repo: &Arc<ReadonlyRepo>, path_contents: &[(&RepoPath, &str)]
     create_tree_with(repo, |builder| {
         for (path, contents) in path_contents {
             builder.file(path, contents);
+        }
+    })
+}
+
+pub fn create_tree_with_copy_history(
+    repo: &Arc<ReadonlyRepo>,
+    path_contents: &[(&RepoPath, &str, &CopyHistory)],
+) -> MergedTree {
+    create_tree_with(repo, |builder| {
+        for (path, contents, history) in path_contents {
+            builder.file(path, contents).copy_history(history);
         }
     })
 }
@@ -770,4 +799,36 @@ pub fn assert_no_forgotten_test_files(test_dir: &Path) {
             .map(|mod_stem| format!("{mod_stem}.rs"))
             .join(", "),
     );
+}
+
+// Creates a CopyHistory graph and writes it to the test repo, returning the
+// resulting IDs and Histories. Items in `paths_and_parents` can refer to parent
+// paths occuring earlier in the list
+pub fn write_copy_histories(
+    repo: &Arc<ReadonlyRepo>,
+    paths_and_parents: &[(&RepoPath, Vec<&RepoPath>)],
+) -> (
+    HashMap<RepoPathBuf, CopyId>,
+    HashMap<RepoPathBuf, CopyHistory>,
+) {
+    let mut ids = HashMap::<RepoPathBuf, CopyId>::new();
+    let mut histories = HashMap::<RepoPathBuf, CopyHistory>::new();
+
+    for (path, parents) in paths_and_parents {
+        let history = CopyHistory {
+            current_path: (*path).to_owned(),
+            parents: parents.iter().map(|parent| ids[*parent].clone()).collect(),
+            salt: vec![],
+        };
+        let id = repo
+            .store()
+            .backend()
+            .write_copy(&history)
+            .block_on()
+            .unwrap();
+        ids.insert((*path).to_owned(), id);
+        histories.insert((*path).to_owned(), history);
+    }
+
+    (ids, histories)
 }

0WD0/jjosh

jj/lib/testutils/src/lib.rs
@@ -31,6 +31,7 @@ use jj_lib::backend::Backend;
 use jj_lib::backend::BackendInitError;
 use jj_lib::backend::ChangeId;
 use jj_lib::backend::CommitId;
+use jj_lib::backend::CopyHistory;
 use jj_lib::backend::CopyId;
 use jj_lib::backend::FileId;
 use jj_lib::backend::MillisSinceEpoch;
@@ -448,6 +449,7 @@ impl TestTreeBuilder {
             path: path.to_owned(),
             contents: contents.as_ref().to_vec(),
             executable: false,
+            copy_history: None,
             copy_id: CopyId::placeholder(),
         }
     }
@@ -482,6 +484,7 @@ pub struct TestTreeFileEntryBuilder<'a> {
     path: RepoPathBuf,
     contents: Vec<u8>,
     executable: bool,
+    copy_history: Option<CopyHistory>,
     copy_id: CopyId,
 }
 
@@ -491,8 +494,19 @@ impl TestTreeFileEntryBuilder<'_> {
         self
     }
 
+    /// Setting an explicit `CopyHistory` overrides any previous `.copy_id()`
+    /// calls.
+    pub fn copy_history(mut self, copy_history: &CopyHistory) -> Self {
+        self.copy_history = Some(copy_history.clone());
+        self.copy_id = CopyId::placeholder();
+        self
+    }
+
+    /// Setting an explicit `CopyId` overrides any previous `.copy_history()`
+    /// calls.
     pub fn copy_id(mut self, copy_id: CopyId) -> Self {
         self.copy_id = copy_id;
+        self.copy_history = None;
         self
     }
 }
@@ -506,12 +520,22 @@ impl Drop for TestTreeFileEntryBuilder<'_> {
             .block_on()
             .unwrap();
         let path = std::mem::replace(&mut self.path, RepoPathBuf::root());
+        let copy_id = if let Some(copy_history) = self.copy_history.as_ref() {
+            self.tree_builder
+                .store()
+                .backend()
+                .write_copy(copy_history)
+                .block_on()
+                .unwrap()
+        } else {
+            self.copy_id.clone()
+        };
         self.tree_builder.set(
             path,
             TreeValue::File {
                 id,
                 executable: self.executable,
-                copy_id: self.copy_id.clone(),
+                copy_id,
             },
         );
     }
@@ -591,6 +615,29 @@ pub fn create_tree(repo: &Arc<ReadonlyRepo>, path_contents: &[(&RepoPath, &str)]
     create_tree_with(repo, |builder| {
         for (path, contents) in path_contents {
             builder.file(path, contents);
+        }
+    })
+}
+
+pub fn create_tree_with_copy_history(
+    repo: &Arc<ReadonlyRepo>,
+    histories: &HashMap<RepoPathBuf, CopyHistory>,
+    path_contents: &[(&RepoPath, &str)],
+) -> MergedTree {
+    create_tree_with(repo, |builder| {
+        for (path, contents) in path_contents {
+            builder.file(path, contents).copy_history(&histories[*path]);
+        }
+    })
+}
+
+pub fn create_tree_with_copy_id(
+    repo: &Arc<ReadonlyRepo>,
+    path_contents_id: &[(&RepoPath, &str, &CopyId)],
+) -> MergedTree {
+    create_tree_with(repo, |builder| {
+        for (path, contents, id) in path_contents_id {
+            builder.file(path, contents).copy_id((*id).clone());
         }
     })
 }
@@ -857,6 +904,35 @@ impl CommitBuilderExt for CommitBuilder<'_> {
     fn write_unwrap(self) -> Commit {
         self.write().block_on().unwrap()
     }
+}
+
+// Creates a CopyHistory graph and writes it to the test repo, returning the
+// resulting IDs and Histories. Items in `paths_and_parents` can refer to parent
+// paths occurring earlier in the list
+pub fn write_copy_histories(
+    repo: &Arc<ReadonlyRepo>,
+    paths_and_parents: &[(&RepoPath, Vec<&RepoPath>)],
+) -> HashMap<RepoPathBuf, CopyHistory> {
+    let mut ids = HashMap::<RepoPathBuf, CopyId>::new();
+    let mut histories = HashMap::<RepoPathBuf, CopyHistory>::new();
+
+    for (path, parents) in paths_and_parents {
+        let history = CopyHistory {
+            current_path: (*path).to_owned(),
+            parents: parents.iter().map(|parent| ids[*parent].clone()).collect(),
+            salt: vec![],
+        };
+        let id = repo
+            .store()
+            .backend()
+            .write_copy(&history)
+            .block_on()
+            .unwrap();
+        ids.insert((*path).to_owned(), id);
+        histories.insert((*path).to_owned(), history);
+    }
+
+    histories
 }
 
 /// Returns true if the directory appears to be on a filesystem with strict