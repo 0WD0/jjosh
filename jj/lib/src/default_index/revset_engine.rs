jj-vcs/jj

revset: add merge_point function
This adds the revset function `merge_point(x)` which computes the point where
all commits in `x` merge together. It is the compliment to the `fork_point(x)`
function and defined as `roots(x_1:: & x_2:: & ... & x_N::)`, where `x_{1..N}`
are the commits in the revset `x`. The merge point of the empty revset is
`none()`, and if `x` is a single commit, then `merge_point(x)` is `x` itself.

Related to https://github.com/jj-vcs/jj/issues/8643

Implemented in https://github.com/jj-vcs/jj/pull/9719

lib/src/default_index/revset_engine.rs
@@ -1005,6 +1005,55 @@ impl EvaluationContext<'_> {
                 }
                 Ok(Box::new(EagerRevset { positions }))
             }
+            ResolvedExpression::MergePoint {
+                expression,
+                visible_heads,
+            } => {
+                let mut root_position_iter = self.evaluate(expression)?.positions().attach(index);
+                let Some(position) = root_position_iter.next() else {
+                    return Ok(Box::new(EagerRevset::empty()));
+                };
+                let visible_head_positions = self
+                    .revset_for_commit_ids(visible_heads)?
+                    .positions()
+                    .attach(index)
+                    .try_collect()?;
+                let mut candidates = RevWalkBuilder::new(index)
+                    .wanted_heads(visible_head_positions)
+                    .descendants(maplit::hashset![position?])
+                    .collect_vec();
+                candidates.reverse();
+                for position in root_position_iter {
+                    let descendants = RevWalkBuilder::new(index)
+                        .wanted_heads(candidates.clone())
+                        .descendants(maplit::hashset![position?])
+                        .collect_vec();
+                    let intersection = intersection_by::<_, _, std::convert::Infallible, _, _, _>(
+                        EagerRevWalk::new(candidates.into_iter().map(Ok)),
+                        EagerRevWalk::new(descendants.into_iter().rev().map(Ok)),
+                        |a, b| a.cmp(b).reverse(),
+                    );
+                    candidates = intersection
+                        .attach(index)
+                        .try_collect()
+                        .unwrap_or_else(|e| match e {});
+                    if candidates.is_empty() {
+                        return Ok(Box::new(EagerRevset::empty()));
+                    }
+                }
+                let candidate_set: HashSet<_> = candidates.iter().copied().collect();
+                candidates.retain(|&pos| {
+                    !index
+                        .commits()
+                        .entry_by_pos(pos)
+                        .parent_positions()
+                        .iter()
+                        .any(|parent| candidate_set.contains(parent))
+                });
+                Ok(Box::new(EagerRevset {
+                    positions: candidates,
+                }))
+            }
             ResolvedExpression::Bisect(candidates) => {
                 let set = self.evaluate(candidates)?;
                 // TODO: Make this more correct in non-linear history

lib/tests/test_revset.rs
@@ -3279,6 +3279,206 @@ fn test_evaluate_expression_fork_point_merge_with_ancestor() {
 }
 
 #[test]
+fn test_evaluate_expression_merge_point() {
+    let test_repo = TestRepo::init();
+    let repo = &test_repo.repo;
+
+    // 7
+    // |\
+    // | |\
+    // 5 6 |
+    // | | |
+    // 4 | |
+    // |\| |
+    // 1 2 3
+    // | |/
+    // |/
+    // 0
+    let mut tx = repo.start_transaction();
+    let mut_repo = tx.repo_mut();
+    let root_commit = repo.store().root_commit();
+    let commit1 = write_random_commit(mut_repo);
+    let commit2 = write_random_commit(mut_repo);
+    let commit3 = write_random_commit(mut_repo);
+    let commit4 = write_random_commit_with_parents(mut_repo, &[&commit1, &commit2]);
+    let commit5 = write_random_commit_with_parents(mut_repo, &[&commit4]);
+    let commit6 = write_random_commit_with_parents(mut_repo, &[&commit2]);
+    let commit7 = write_random_commit_with_parents(mut_repo, &[&commit5, &commit6, &commit3]);
+
+    assert_eq!(resolve_commit_ids(mut_repo, "merge_point(none())"), vec![]);
+    assert_eq!(
+        resolve_commit_ids(mut_repo, "merge_point(all())"),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, "merge_point(root())"),
+        vec![root_commit.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit1.id())),
+        vec![commit1.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit2.id())),
+        vec![commit2.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit3.id())),
+        vec![commit3.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit4.id())),
+        vec![commit4.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit5.id())),
+        vec![commit5.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit6.id())),
+        vec![commit6.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit5.id(), commit6.id())
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit6.id(), commit3.id())
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!(
+                "merge_point({} | {} | {})",
+                commit5.id(),
+                commit6.id(),
+                commit3.id()
+            )
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit5.id(), commit4.id())
+        ),
+        vec![commit5.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit6.id(), commit1.id())
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit3.id(), commit2.id())
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit5.id(), commit1.id())
+        ),
+        vec![commit5.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit4.id(), commit1.id())
+        ),
+        vec![commit4.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit1.id(), commit2.id())
+        ),
+        vec![commit4.id().clone()]
+    );
+}
+
+#[test]
+fn test_evaluate_expression_merge_point_criss_cross() {
+    let test_repo = TestRepo::init();
+    let repo = &test_repo.repo;
+
+    // 3 4
+    // |X|
+    // 1 2
+    // |/
+    // 0
+    let mut tx = repo.start_transaction();
+    let mut_repo = tx.repo_mut();
+    let commit1 = write_random_commit(mut_repo);
+    let commit2 = write_random_commit(mut_repo);
+    let commit3 = write_random_commit_with_parents(mut_repo, &[&commit1, &commit2]);
+    let commit4 = write_random_commit_with_parents(mut_repo, &[&commit1, &commit2]);
+
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit1.id(), commit2.id())
+        ),
+        vec![commit4.id().clone(), commit3.id().clone()]
+    );
+
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit3.id(), commit4.id())
+        ),
+        vec![]
+    );
+}
+
+#[test]
+fn test_evaluate_expression_merge_point_with_descendants() {
+    let test_repo = TestRepo::init();
+    let repo = &test_repo.repo;
+
+    //   6   7
+    //    \ /
+    // 3   4   5
+    //  \ / \ /
+    //   1   2
+    //    \ /
+    //     0
+    let mut tx = repo.start_transaction();
+    let mut_repo = tx.repo_mut();
+    let commit1 = write_random_commit(mut_repo);
+    let commit2 = write_random_commit(mut_repo);
+    let commit3 = write_random_commit_with_parents(mut_repo, &[&commit1]);
+    let commit4 = write_random_commit_with_parents(mut_repo, &[&commit1, &commit2]);
+    let commit5 = write_random_commit_with_parents(mut_repo, &[&commit2]);
+    let commit6 = write_random_commit_with_parents(mut_repo, &[&commit4]);
+    let commit7 = write_random_commit_with_parents(mut_repo, &[&commit4]);
+
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit1.id(), commit2.id())
+        ),
+        vec![commit4.id().clone()]
+    );
+
+    drop(commit3);
+    drop(commit5);
+    drop(commit6);
+    drop(commit7);
+}
+
+#[test]
 fn test_evaluate_expression_exactly() {
     let test_repo = TestRepo::init();
     let repo = &test_repo.repo;

0WD0/jjosh

jj/lib/src/default_index/revset_engine.rs
@@ -1005,6 +1005,47 @@ impl EvaluationContext<'_> {
                 }
                 Ok(Box::new(EagerRevset { positions }))
             }
+            ResolvedExpression::MergePoint {
+                roots,
+                visible_heads,
+            } => {
+                let mut root_position_iter = self.evaluate(roots)?.positions().attach(index);
+                let Some(position) = root_position_iter.next() else {
+                    return Ok(Box::new(EagerRevset::empty()));
+                };
+                let visible_head_positions = self
+                    .evaluate(visible_heads)?
+                    .positions()
+                    .attach(index)
+                    .try_collect()?;
+                let mut candidates = RevWalkBuilder::new(index)
+                    .wanted_heads(visible_head_positions)
+                    .descendants(maplit::hashset![position?])
+                    .collect_vec();
+                candidates.reverse();
+                for position in root_position_iter {
+                    let descendants = RevWalkBuilder::new(index)
+                        .wanted_heads(candidates.clone())
+                        .descendants(maplit::hashset![position?])
+                        .collect_positions_set();
+                    candidates.retain(|pos| descendants.contains(pos));
+                    if candidates.is_empty() {
+                        return Ok(Box::new(EagerRevset::empty()));
+                    }
+                }
+                let candidate_set: HashSet<_> = candidates.iter().copied().collect();
+                candidates.retain(|&pos| {
+                    !index
+                        .commits()
+                        .entry_by_pos(pos)
+                        .parent_positions()
+                        .iter()
+                        .any(|parent| candidate_set.contains(parent))
+                });
+                Ok(Box::new(EagerRevset {
+                    positions: candidates,
+                }))
+            }
             ResolvedExpression::Bisect(candidates) => {
                 let set = self.evaluate(candidates)?;
                 // TODO: Make this more correct in non-linear history

jj/lib/tests/test_revset.rs
@@ -3279,6 +3279,201 @@ fn test_evaluate_expression_fork_point_merge_with_ancestor() {
 }
 
 #[test]
+fn test_evaluate_expression_merge_point() {
+    let test_repo = TestRepo::init();
+    let repo = &test_repo.repo;
+
+    // 7
+    // |\
+    // | |\
+    // 5 6 |
+    // | | |
+    // 4 | |
+    // |\| |
+    // 1 2 3
+    // | |/
+    // |/
+    // 0
+    let mut tx = repo.start_transaction();
+    let mut_repo = tx.repo_mut();
+    let root_commit = repo.store().root_commit();
+    let commit1 = write_random_commit(mut_repo);
+    let commit2 = write_random_commit(mut_repo);
+    let commit3 = write_random_commit(mut_repo);
+    let commit4 = write_random_commit_with_parents(mut_repo, &[&commit1, &commit2]);
+    let commit5 = write_random_commit_with_parents(mut_repo, &[&commit4]);
+    let commit6 = write_random_commit_with_parents(mut_repo, &[&commit2]);
+    let commit7 = write_random_commit_with_parents(mut_repo, &[&commit5, &commit6, &commit3]);
+
+    assert_eq!(resolve_commit_ids(mut_repo, "merge_point(none())"), vec![]);
+    assert_eq!(
+        resolve_commit_ids(mut_repo, "merge_point(all())"),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, "merge_point(root())"),
+        vec![root_commit.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit1.id())),
+        vec![commit1.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit2.id())),
+        vec![commit2.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit3.id())),
+        vec![commit3.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit4.id())),
+        vec![commit4.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit5.id())),
+        vec![commit5.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(mut_repo, &format!("merge_point({})", commit6.id())),
+        vec![commit6.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit5.id(), commit6.id())
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit6.id(), commit3.id())
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!(
+                "merge_point({} | {} | {})",
+                commit5.id(),
+                commit6.id(),
+                commit3.id()
+            )
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit5.id(), commit4.id())
+        ),
+        vec![commit5.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit6.id(), commit1.id())
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit3.id(), commit2.id())
+        ),
+        vec![commit7.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit5.id(), commit1.id())
+        ),
+        vec![commit5.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit4.id(), commit1.id())
+        ),
+        vec![commit4.id().clone()]
+    );
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit1.id(), commit2.id())
+        ),
+        vec![commit4.id().clone()]
+    );
+}
+
+#[test]
+fn test_evaluate_expression_merge_point_criss_cross() {
+    let test_repo = TestRepo::init();
+    let repo = &test_repo.repo;
+
+    // 3 4
+    // |X|
+    // 1 2
+    // |/
+    // 0
+    let mut tx = repo.start_transaction();
+    let mut_repo = tx.repo_mut();
+    let commit1 = write_random_commit(mut_repo);
+    let commit2 = write_random_commit(mut_repo);
+    let commit3 = write_random_commit_with_parents(mut_repo, &[&commit1, &commit2]);
+    let commit4 = write_random_commit_with_parents(mut_repo, &[&commit1, &commit2]);
+
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit1.id(), commit2.id())
+        ),
+        vec![commit4.id().clone(), commit3.id().clone()]
+    );
+
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit3.id(), commit4.id())
+        ),
+        vec![]
+    );
+}
+
+#[test]
+fn test_evaluate_expression_merge_point_with_descendants() {
+    let test_repo = TestRepo::init();
+    let repo = &test_repo.repo;
+
+    //   6   7
+    //    \ /
+    // 3   4   5
+    //  \ / \ /
+    //   1   2
+    //    \ /
+    //     0
+    let mut tx = repo.start_transaction();
+    let mut_repo = tx.repo_mut();
+    let commit1 = write_random_commit(mut_repo);
+    let commit2 = write_random_commit(mut_repo);
+    let commit4 = write_random_commit_with_parents(mut_repo, &[&commit1, &commit2]);
+    let _commit3 = write_random_commit_with_parents(mut_repo, &[&commit1]);
+    let _commit5 = write_random_commit_with_parents(mut_repo, &[&commit2]);
+    let _commit6 = write_random_commit_with_parents(mut_repo, &[&commit4]);
+    let _commit7 = write_random_commit_with_parents(mut_repo, &[&commit4]);
+
+    assert_eq!(
+        resolve_commit_ids(
+            mut_repo,
+            &format!("merge_point({} | {})", commit1.id(), commit2.id())
+        ),
+        vec![commit4.id().clone()]
+    );
+}
+
+#[test]
 fn test_evaluate_expression_exactly() {
     let test_repo = TestRepo::init();
     let repo = &test_repo.repo;