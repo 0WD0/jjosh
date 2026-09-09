jj-vcs/jj

cli/run: add option to set sparse patterns
This uses previously exposed enum SparseInheritance
to add a familiar interface for setting sparse
patterns in the "workspace" in which the command is
run. This gives the user an option to forego expensive
materializations when unneeded.

For example, if a user only needs to run a command
only for two files `bar` and `foo`, they can now do
so with first setting the sparse pattern for the
parent workspace, as jj run inheriths sparse patterns
by default:

```
jj sparse set --clear --add bar --add foo
jj run -- <command>
```

Conversely, this also allows `jj run` to execute in
a fully materialized workspace, despite its parent
having sparse patterns set:

```
jj run --sparse-args full -- <command>
```

Or lastly, they might opt in running a command in
completely empty workspace:

```
jj run --sparse-args empty -- <command>
```

cli/src/commands/run.rs
@@ -54,6 +54,7 @@ use jj_lib::merge::Merge;
 use jj_lib::merged_tree::MergedTree;
 use jj_lib::object_id::ObjectId as _;
 use jj_lib::repo::Repo as _;
+use jj_lib::repo_path::RepoPathBuf;
 use jj_lib::working_copy::SnapshotOptions;
 use tokio::runtime::Builder;
 use tokio::sync::mpsc;
@@ -68,6 +69,7 @@ use crate::cli_util::WorkspaceCommandHelper;
 use crate::cli_util::WorkspaceCommandTransaction;
 use crate::command_error::CommandError;
 use crate::command_error::CommandErrorKind;
+use crate::commands::workspace::SparseInheritance;
 use crate::ui::Ui;
 
 #[derive(Debug, thiserror::Error)]
@@ -167,6 +169,9 @@ struct WorkspacePool {
     /// When true, wipe each slot's working copy on acquisition so every commit
     /// starts from a freshly checked-out tree (no artifact reuse).
     clean: bool,
+    /// Sparse patterns that influence what parts of the tree are materialized
+    /// when running the command. If None, whole tree is materialized.
+    sparsity: Option<Vec<RepoPathBuf>>,
 }
 
 impl WorkspacePool {
@@ -175,6 +180,7 @@ impl WorkspacePool {
         size: NonZeroUsize,
         auto_tracking_matcher: Box<dyn Matcher>,
         clean: bool,
+        sparsity: Option<Vec<RepoPathBuf>>,
     ) -> Result<Self, RunError> {
         // The parent() call is needed to not write under `.jj/repo/`.
         let base_path = repo_path.parent().unwrap().join("run").join("default");
@@ -184,6 +190,7 @@ impl WorkspacePool {
             size,
             auto_tracking_matcher,
             clean,
+            sparsity,
         })
     }
 
@@ -257,6 +264,19 @@ impl WorkspacePool {
                 &settings,
             )
         };
+
+        // TODO: define a separate error for failing to set sparse patterns.
+        if let Some(sparse_patterns) = &self.sparsity {
+            tree_state
+                .set_sparse_patterns(sparse_patterns.clone())
+                .map_err(|_| RunError::FailedCheckout(commit.id().clone()))?;
+        } else {
+            // Users can specify `--sparse-args full` to materialize whole tree,
+            // without having to clear the run "workspace" first with `--clean`.
+            tree_state
+                .set_sparse_patterns(vec![RepoPathBuf::root()])
+                .map_err(|_| RunError::FailedCheckout(commit.id().clone()))?;
+        }
 
         tree_state
             .check_out(&commit.tree())
@@ -666,6 +686,16 @@ pub struct RunArgs {
     /// `jj run` itself.
     #[arg(long)]
     ignore_errors: bool,
+
+    /// How to handle sparse patterns when running the command. This is
+    /// controlled per `jj run` invocation.
+    ///
+    /// If sparse patterns were inherited in a run and a subsequent run is
+    /// passed `--sparse-args full`, but no `--clean`, then the command will
+    /// run in fully materialized workspace together with all artifacts left
+    /// from the previous run.
+    #[arg(long, value_enum, default_value_t = SparseInheritance::Copy)]
+    sparse_args: SparseInheritance,
 }
 
 /// Precedence: `--jobs`, `run.jobs` config, 1.
@@ -759,6 +789,15 @@ pub async fn cmd_run(
     let store = workspace_command.repo().store().clone();
     let auto_tracking_matcher = workspace_command.auto_tracking_matcher(ui)?;
 
+    let sparsity = match args.sparse_args {
+        SparseInheritance::Full => None,
+        SparseInheritance::Empty => Some(vec![]),
+        SparseInheritance::Copy => {
+            let sparse_patterns = workspace_command.working_copy().sparse_patterns()?.to_vec();
+            Some(sparse_patterns)
+        }
+    };
+
     let mut tx = workspace_command.start_transaction();
 
     let rt = {
@@ -775,6 +814,7 @@ pub async fn cmd_run(
         jobs,
         auto_tracking_matcher,
         args.clean,
+        sparsity,
     )?);
 
     let spec = Arc::new(CommandSpec {

cli/tests/test_run_command.rs
@@ -1980,3 +1980,100 @@ fn test_run_on_conflicted_commit() {
     [EOF]
     ");
 }
+
+#[test]
+fn test_run_sparse() {
+    let test_env = TestEnvironment::default();
+    test_env.run_jj_in(".", ["git", "init", "repo"]).success();
+    let work_dir = test_env.work_dir("repo");
+    work_dir.write_file("a", "");
+    work_dir.run_jj(&["commit", "-m", "A"]).success();
+    work_dir.write_file("b", "");
+    work_dir.run_jj(&["commit", "-m", "B"]).success();
+    work_dir.write_file("c", "");
+    work_dir.run_jj(&["commit", "-m", "C"]).success();
+    insta::assert_snapshot!(get_log_output(&work_dir), @r"
+    @  zsuskulnrvyrovkzqrwmxqlsskqntxvp
+    ○  kkmpptxzrspxrzommnulwmwkkqwworplC
+    │
+    ○  rlvkpnrzqnoowoytxnquwvuryrwnrmlpB
+    │
+    ○  qpvuntsmwlqtpsluzzsnyyzlmlwvmlnuA
+    │
+    ◆  zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+    [EOF]
+    ");
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @r"
+        a
+        a
+        b
+        a
+        b
+        c
+        a
+        b
+        c
+        [EOF]
+        ");
+
+    work_dir
+        .run_jj(&["sparse", "set", "--clear", "--add", "a", "--add", "b"])
+        .success();
+
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @r"
+        a
+        a
+        b
+        a
+        b
+        a
+        b
+        [EOF]
+        ");
+
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--sparse-args", "copy", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @r"
+        a
+        a
+        b
+        a
+        b
+        a
+        b
+        [EOF]
+        ");
+
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--sparse-args", "empty", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @"");
+
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--sparse-args", "full", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @r"
+        a
+        a
+        b
+        a
+        b
+        c
+        a
+        b
+        c
+        [EOF]
+        ");
+}

0WD0/jjosh

jj/cli/src/commands/run.rs
@@ -53,6 +53,7 @@ use jj_lib::merge::Merge;
 use jj_lib::merged_tree::MergedTree;
 use jj_lib::object_id::ObjectId as _;
 use jj_lib::repo::Repo as _;
+use jj_lib::repo_path::RepoPathBuf;
 use jj_lib::working_copy::SnapshotOptions;
 use tokio::runtime::Builder;
 use tokio::sync::mpsc;
@@ -67,6 +68,7 @@ use crate::cli_util::WorkspaceCommandHelper;
 use crate::cli_util::WorkspaceCommandTransaction;
 use crate::command_error::CommandError;
 use crate::command_error::CommandErrorKind;
+use crate::commands::workspace::SparseInheritance;
 use crate::ui::Ui;
 
 #[derive(Debug, thiserror::Error)]
@@ -166,6 +168,9 @@ struct WorkspacePool {
     /// When true, wipe each slot's working copy on acquisition so every commit
     /// starts from a freshly checked-out tree (no artifact reuse).
     clean: bool,
+    /// Sparse patterns that influence what parts of the tree are materialized
+    /// when running the command. If None, whole tree is materialized.
+    sparsity: Option<Vec<RepoPathBuf>>,
 }
 
 impl WorkspacePool {
@@ -174,6 +179,7 @@ impl WorkspacePool {
         size: NonZeroUsize,
         auto_tracking_matcher: Box<dyn Matcher>,
         clean: bool,
+        sparsity: Option<Vec<RepoPathBuf>>,
     ) -> Result<Self, RunError> {
         // The parent() call is needed to not write under `.jj/repo/`.
         let base_path = repo_path.parent().unwrap().join("run").join("default");
@@ -183,6 +189,7 @@ impl WorkspacePool {
             size,
             auto_tracking_matcher,
             clean,
+            sparsity,
         })
     }
 
@@ -256,6 +263,18 @@ impl WorkspacePool {
                 &settings,
             )
         };
+
+        if let Some(sparse_patterns) = &self.sparsity {
+            tree_state
+                .set_sparse_patterns(sparse_patterns.clone())
+                .map_err(|_| RunError::FailedCheckout(commit.id().clone()))?;
+        } else {
+            // Users can specify `--sparse-args full` to materialize whole tree,
+            // without having to clear the run "workspace" first with `--clean`.
+            tree_state
+                .set_sparse_patterns(vec![RepoPathBuf::root()])
+                .map_err(|_| RunError::FailedCheckout(commit.id().clone()))?;
+        }
 
         tree_state
             .check_out(&commit.tree())
@@ -661,6 +680,16 @@ pub struct RunArgs {
     /// `jj run` itself.
     #[arg(long)]
     ignore_errors: bool,
+
+    /// How to handle sparse patterns when running the command. This is
+    /// controlled per `jj run` invocation.
+    ///
+    /// If sparse patterns were inherited in a run and a subsequent run is
+    /// passed `--sparse-args full`, but no `--clean`, then the command will
+    /// run in fully materialized workspace together with all artifacts left
+    /// from the previous run.
+    #[arg(long, value_enum, default_value_t = SparseInheritance::Copy)]
+    sparse_patterns: SparseInheritance,
 }
 
 /// Precedence: `--jobs`, `run.jobs` config, 1.
@@ -754,6 +783,15 @@ pub async fn cmd_run(
     let store = workspace_command.repo().store().clone();
     let auto_tracking_matcher = workspace_command.auto_tracking_matcher(ui)?;
 
+    let sparsity = match args.sparse_patterns {
+        SparseInheritance::Full => None,
+        SparseInheritance::Empty => Some(vec![]),
+        SparseInheritance::Copy => {
+            let sparse_patterns = workspace_command.working_copy().sparse_patterns()?.to_vec();
+            Some(sparse_patterns)
+        }
+    };
+
     let mut tx = workspace_command.start_transaction();
 
     let rt = {
@@ -769,6 +807,7 @@ pub async fn cmd_run(
         jobs,
         auto_tracking_matcher,
         args.clean,
+        sparsity,
     )?);
 
     let spec = Arc::new(CommandSpec {

jj/cli/tests/test_run_command.rs
@@ -2083,3 +2083,100 @@ fn test_run_multi_commit_partial_diff() {
         .to_string();
     assert_eq!(log_commit_a_before, log_commit_a_after);
 }
+
+#[test]
+fn test_run_sparse() {
+    let test_env = TestEnvironment::default();
+    test_env.run_jj_in(".", ["git", "init", "repo"]).success();
+    let work_dir = test_env.work_dir("repo");
+    work_dir.write_file("a", "");
+    work_dir.run_jj(&["commit", "-m", "A"]).success();
+    work_dir.write_file("b", "");
+    work_dir.run_jj(&["commit", "-m", "B"]).success();
+    work_dir.write_file("c", "");
+    work_dir.run_jj(&["commit", "-m", "C"]).success();
+    insta::assert_snapshot!(get_log_output(&work_dir), @r"
+    @  zsuskulnrvyrovkzqrwmxqlsskqntxvp
+    ○  kkmpptxzrspxrzommnulwmwkkqwworplC
+    │
+    ○  rlvkpnrzqnoowoytxnquwvuryrwnrmlpB
+    │
+    ○  qpvuntsmwlqtpsluzzsnyyzlmlwvmlnuA
+    │
+    ◆  zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+    [EOF]
+    ");
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @r"
+        a
+        a
+        b
+        a
+        b
+        c
+        a
+        b
+        c
+        [EOF]
+        ");
+
+    work_dir
+        .run_jj(&["sparse", "set", "--clear", "--add", "a", "--add", "b"])
+        .success();
+
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @r"
+        a
+        a
+        b
+        a
+        b
+        a
+        b
+        [EOF]
+        ");
+
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--sparse-patterns", "copy", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @r"
+        a
+        a
+        b
+        a
+        b
+        a
+        b
+        [EOF]
+        ");
+
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--sparse-patterns", "empty", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @"");
+
+    let stdout = work_dir
+        .run_jj(&["run", "-r", "..@", "--sparse-patterns", "full", "--", "ls"])
+        .success()
+        .stdout;
+    insta::assert_snapshot!(stdout, @r"
+        a
+        a
+        b
+        a
+        b
+        c
+        a
+        b
+        c
+        [EOF]
+        ");
+}